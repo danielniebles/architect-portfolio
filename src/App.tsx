10928d1cdd316ee1fea2a871ad3414aef63fdfab
danielniebles <danielf.niebles@gmail.com>
2022-07-13 10:41:42 -0500

    chore: add icons and contact info

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
-import { useState } from 'react'
-import logo from '../assets/main_logo.svg'
-import './App.css'
-import './index.css'
+import { useState } from "react";
+import logo from "../assets/main_logo.svg";
+import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <header className="App-header">
-        <h4 style={{fontWeight: 700}}>Página en construcción...</h4>
+        <h4 style={{ fontWeight: 700 }}>Página en construcción...</h4>
         <img src={logo} className="App-logo" alt="logo" />
+
+        <div className="info__container">
+          <div className="contact__item">
+            <i style={{marginRight: '16px'}} className="uil uil-envelope"></i>
+            <p className="text">javier.niebles@gmail.com</p>
+          </div>
+          <div className="contact__item">
+            <i style={{marginRight: '16px'}} className="uil uil-whatsapp"></i>
+            <a
+              className="text link"
+              href="https://wa.link/ozr341"
+              target="_blank"
+            >
+              (+57) 300 373 4489
+            </a>
+          </div>
+        </div>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
