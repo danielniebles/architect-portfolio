04466c872d2d5cd9403f30afa8bec2b8372748bf
danielniebles <danielf.niebles@gmail.com>
2022-07-24 19:06:37 -0500

    chore: create services section and modify base components

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,7 @@
 import Children from '../interfaces/Children';
 
-const SectionTitle = ({ children }: Children): JSX.Element => {
-  return <h2 className="text-lg md:text-xl font-semibold">{children}</h2>;
+const Title = ({ children, customClass }: Children): JSX.Element => {
+  return <h2 className={`text-lg md:text-xl font-semibold ${customClass}`}>{children}</h2>;
 };
 
-export default SectionTitle;
+export default Title;
