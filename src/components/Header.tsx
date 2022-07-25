f9ebee7bb9cd3fc58ba42ba49dcd3c96314f7b1d
danielniebles <danielf.niebles@gmail.com>
2022-07-24 19:07:51 -0500

    chore: add footer and modify header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,20 +21,22 @@ const Header = (): JSX.Element => {
     return () => window.removeEventListener('scroll', checkScrollOffset);
   }, []);
 
+  //TODO: Add hover styles and links
   return (
     <header
       className={`fixed top-0 z-10 h-12 w-full transition duration-500 ${
-        isScrolled ? 'bg-light-blue-900' : 'border-b bg-transparent'
+        isScrolled ? 'bg-light-blue-900 shadow-xl' : 'border-b bg-transparent'
       }`}
     >
       <nav className="flex h-12 items-center justify-between">
         <img src="assets/logo.svg" className="h-6 ml-1"></img>
         <div
-          className={`fixed flex w-full md:w-1/3 flex-col text-gray-200 md:relative ${
+          className={`fixed flex w-full md:w-1/4 flex-col text-gray-200 md:relative ${
             showMenu ? 'top-0' : '-top-full'
           } transition-all duration-500 md:top-0`}
         >
           <img src="assets/logo.svg" className="absolute top-2 left-2 h-6"></img>
+
           <ul className="flex flex-col items-center gap-2 bg-light-blue-900 md:bg-transparent p-10 md:flex-row md:justify-between">
             <li className="p-1">Home</li>
             <li className="p-1">Contacto</li>
@@ -45,7 +47,7 @@ const Header = (): JSX.Element => {
             <i className="uil uil-times text-xl font-normal text-gray-200"></i>
           </button>
         </div>
-        <button className='md:hidden mr-1' onClick={() => handleShowMenu()}>
+        <button className='md:hidden mr-4' onClick={() => handleShowMenu()}>
           <i className="uil uil-bars text-2xl font-normal text-gray-200"></i>
         </button>
       </nav>
