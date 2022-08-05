bdd7d5e13b17f758a45874aab316f5c864f675f1
danielniebles <danielf.niebles@gmail.com>
2022-08-05 18:09:46 -0500

    chore: add terra log

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logo from "../assets/main_logo.svg";
+import logoTerra from "../assets/logo_terra.svg";
 import "./App.css";
 import "./index.css";
 
@@ -10,12 +11,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h4 style={{ fontWeight: 700 }}>Página en construcción...</h4>
+        <div style={{ display: 'flex'}}>
+
         <img src={logo} className="App-logo" alt="logo" />
+        <div style={{borderLeft:'3px solid #454b4d', marginRight: '20px', marginLeft: '20px', backgroundColor: 'red'}}></div>
+        <img src={logoTerra} className="App-logo" alt="logo" />
+        </div>
 
         <div className="info__container">
           <div className="contact__item">
             <i style={{marginRight: '16px'}} className="uil uil-envelope"></i>
-            <p className="text">javier.niebles@gmail.com</p>
+            <p className="text">contacto@javierniebles.com</p>
           </div>
           <div className="contact__item">
             <i style={{marginRight: '16px'}} className="uil uil-whatsapp"></i>
@@ -24,7 +30,7 @@ function App() {
               href="https://wa.link/ozr341"
               target="_blank"
             >
-              (+57) 300 373 4489
+              (+57) 311 398 8081
             </a>
           </div>
         </div>
