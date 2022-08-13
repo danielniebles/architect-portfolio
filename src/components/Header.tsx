82de22423400381afc468c9ab35117e72f6326e5
danielniebles <danielf.niebles@gmail.com>
2022-08-13 12:55:11 -0500

    chore: fix header behavior and styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,26 +28,34 @@ const Header = (): JSX.Element => {
         isScrolled ? 'bg-light-blue-900 shadow-xl' : 'border-b bg-transparent'
       }`}
     >
-      <nav className="flex h-12 items-center justify-between">
-        <img src="assets/logo.svg" className="h-6 ml-1"></img>
+      <nav className="flex h-12 w-full items-center justify-between shadow-xl">
+        <img src="assets/logo.svg" className="ml-4 h-6"></img>
         <div
-          className={`fixed flex w-full md:w-1/4 flex-col text-gray-200 md:relative ${
+          className={`fixed flex w-full flex-col text-gray-200 md:relative md:w-2/4 ${
             showMenu ? 'top-0' : '-top-full'
-          } transition-all duration-500 md:top-0`}
+          } transition-all duration-500 md:top-0 md:mx-4 shadow-xl md:shadow-none`}
         >
-          <img src="assets/logo.svg" className="absolute top-2 left-2 h-6"></img>
-
-          <ul className="flex flex-col items-center gap-2 bg-light-blue-900 md:bg-transparent p-10 md:flex-row md:justify-between">
+          <img
+            src="assets/logo.svg"
+            className="absolute top-2 left-2 h-6 md:hidden"
+          ></img>
+          <ul
+            className="flex flex-col items-center gap-2 bg-light-blue-900
+            p-10 md:flex-row md:justify-end md:bg-transparent md:p-0"
+          >
             <li className="p-1">Home</li>
             <li className="p-1">Contacto</li>
             <li className="p-1">Portafolio</li>
             <li className="p-1">Servicios</li>
           </ul>
-          <button className="absolute top-1 right-1" onClick={() => handleCloseMenu()}>
+          <button
+            className="absolute top-1 right-1 md:hidden"
+            onClick={() => handleCloseMenu()}
+          >
             <i className="uil uil-times text-xl font-normal text-gray-200"></i>
           </button>
         </div>
-        <button className='md:hidden mr-4' onClick={() => handleShowMenu()}>
+        <button className="mr-4 md:hidden" onClick={() => handleShowMenu()}>
           <i className="uil uil-bars text-2xl font-normal text-gray-200"></i>
         </button>
       </nav>
