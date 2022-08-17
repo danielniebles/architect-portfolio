4cdb9805aba156fae1cde8f5d2693cfa64a62571
danielniebles <danielf.niebles@gmail.com>
2022-08-17 18:26:54 -0500

    chore: add onClick handler for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,9 @@
 import Children from '../interfaces/Children';
 
-const PrimaryButton = ({ children }: Children): JSX.Element => (
-  <button className="rounded bg-light-blue-900 py-2 px-4 text-base font-medium text-gray-50 shadow-sm hover:bg-light-blue hover:shadow-lg">
+const PrimaryButton = ({ children, onClick }: Children): JSX.Element => (
+  <button className="rounded bg-light-blue-900 py-2 px-4 text-base
+    font-medium text-gray-50 shadow-sm hover:bg-light-blue hover:shadow-lg"
+    onClick={onClick}>
     {children}
   </button>
 );
