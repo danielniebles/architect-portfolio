ee7fd7a8cc462ad49c5b805d6e4ebb17489a2d88
danielniebles <danielf.niebles@gmail.com>
2022-08-19 21:06:49 -0500

    chore: improve services information display

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,32 +1,51 @@
-import { useEffect } from 'react';
-import useHover from '../hooks/useHover';
+import { useState } from 'react';
 import Children from '../interfaces/Children';
-import PrimaryButton from './Button';
+import LinkButton from './LinkButton';
 import Title from './Title';
+import './index.css';
 
-const Card = ({ imageUrl, title, textBackground }: Children): JSX.Element => {
-  const [ref, hovered] = useHover();
+const Card = ({ imageUrl, title }: Children): JSX.Element => {
+  const [visibleInfo, setVisibleInfo] = useState(false);
+
+  const onClick = () => setVisibleInfo(true);
+  const onClose = () => setVisibleInfo(false);
 
   return (
-    <div ref={ref} className="relative">
+    <div className="relative">
       <div className="flex h-[60vh] items-center justify-center overflow-hidden">
         <img src={imageUrl} className="h-full w-full object-cover"></img>
       </div>
 
-      <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-60">
-        {!hovered && (
-          <Title
-            customClass={`text-white bg-opacity-40 bg-black w-full text-center mb-25 py-5`}
-          >
-            {title}
-          </Title>
-        )}
-      </div>
-      {hovered && (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <PrimaryButton>Ver más</PrimaryButton>
+      <div className="absolute inset-0 items-center bg-black bg-opacity-60">
+        <div
+          className={`flex ${visibleInfo ? 'h-full' : 'mt-5 h-20'}
+        relative w-full flex-col items-center justify-center
+        bg-black bg-opacity-40 transition-all duration-300 `}
+        >
+          <div className="service__title" onClick={onClick}>
+            <Title customClass={`text-white text-center mt-25 py-5`}>{title}</Title>
+            {!visibleInfo && (
+              <i className="uil uil-arrow-right service__icon ml-2 text-2xl text-white"></i>
+            )}
+          </div>
+          <div className={`${visibleInfo ? 'description__container' : 'hidden'}
+            flex flex-col items-center p-4`}>
+            <p
+              className={` text-white
+              transition duration-1000 text-center`}
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod numquam ipsum
+              consequatur rem aliquid commodi voluptatum, nemo vitae. Cum laudantium optio
+              tempora hic amet. Veniam voluptates quibusdam iure ad enim?
+            </p>
+            <i
+              className="uil uil-times absolute top-1.5 right-1.5 text-2xl text-white"
+              onClick={onClose}
+            ></i>
+            <LinkButton customClass='mt-10'>Ver más</LinkButton>
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 };
